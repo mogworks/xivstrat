c8c315606c7343765e524e11d0602e4dc743b2a8
Super Jump <superjump.void@gmail.com>
2025-06-05 20:32:10 +0800

    xreign 6/n

--- src/pages/07/m8s1/_translations/en.ts
+++
@@ -1,5 +0,0 @@
-export const translations = {
-  revolutionaryReign: 'Revolutionary Reign',
-  eminentReign: 'Eminent Reign',
-  wolvesReign: 'Wolves\' Reign',
-}
---
+++ src/pages/07/m8s1/_translations/jp.ts
@@ -0,0 +1,8 @@
+export const translations = {
+  extraplanarPursuit: '空間斬り',
+  stonefang: '土の魔技',
+  windfang: '风の魔技',
+  revolutionaryReign: '廻の郡狼剣',
+  eminentReign: '薙の郡狼剣',
+  wolvesReign: '郡狼剣',
+}
