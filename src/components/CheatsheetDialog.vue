3632a3a6baa411d8900401a094f099cb437f59f0
Kitano <kitano@influx.io>
2025-06-16 11:42:14 +0800

    feat: homepage for m6s

--- src/components/CheatsheetDialog.vue
+++ src/components/CheatsheetDialog.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+
 import ScannerSVG from '@/assets/svg/scanner.svg?component'
 import {
   Dialog,
@@ -15,6 +17,8 @@ defineProps<{
   description?: string
   src?: string
 }>()
+
+const scale = ref(1)
 </script>
 
 <template>
@@ -29,10 +33,23 @@ defineProps<{
     </DialogTrigger>
     <DialogContent class="w-[80%]">
       <DialogHeader>
-        <DialogTitle v-if="$slots.title || title">
+        <DialogTitle>
           <slot name="title">
             {{ title }}
           </slot>
+          <div class="flex items-center gap-4">
+            <input
+              v-model="scale"
+              type="range"
+              min="0.1"
+              max="3"
+              step="0.01"
+              class="slider inline-flex h-2 max-w-50 flex-grow-1 appearance-none rounded-[4px] bg-[#e0e0e0] outline-none"
+            >
+            <div class="inline-flex min-w-[100px] text-center font-bold">
+              缩放比例: {{ Math.round(scale * 100) }}%
+            </div>
+          </div>
         </DialogTitle>
         <DialogDescription v-if="$slots.description">
           <slot name="description">
@@ -41,7 +58,17 @@ defineProps<{
         </DialogDescription>
       </DialogHeader>
       <slot>
-        <img :src="src" loading="lazy" alt="副本小抄" class="w-full">
+        <div class="overflow-auto">
+          <div
+            class="max-h-[90vh] max-w-[80vw]"
+            :style="{
+              transform: `scale(${scale})`,
+              transformOrigin: 'top left',
+            }"
+          >
+            <img :src="src" loading="lazy" alt="副本小抄" class="block w-full transition-all duration-150">
+          </div>
+        </div>
       </slot>
       <DialogFooter v-if="$slots.footer">
         <slot name="footer" />
@@ -49,3 +76,25 @@ defineProps<{
     </DialogContent>
   </Dialog>
 </template>
+
+<style scoped>
+/* Webkit（Chrome, Safari, Edge） thumb */
+.slider::-webkit-slider-thumb {
+  -webkit-appearance: none;
+  width: 20px;
+  height: 20px;
+  background: #42b883;
+  border-radius: 50%;
+  cursor: pointer;
+}
+
+/* Firefox thumb */
+.slider::-moz-range-thumb {
+  width: 20px;
+  height: 20px;
+  background: #42b883;
+  border-radius: 50%;
+  cursor: pointer;
+  border: none; /* 移除Firefox默认边框 */
+}
+</style>
