21777a60fd86994befc4cedfa369edd489421668
KitanoNani <736818560@qq.com>
2025-06-18 00:53:45 +0800

    feat: developers update

--- src/pages/07/zelenia/_data/thanks.ts
+++ src/pages/07/zelenia/_data/thanks.ts
@@ -1,5 +1,5 @@
 export const thanksList = {
   groupList: ['MMW', 'SPJP'],
-  devList: ['SuperJump', 'DavidKitano', 'LuoLuoKe', 'Sui'],
+  devList: ['SuperJump', 'Hikaru', 'DavidKitano', 'LuoLuoKe', 'Sui'],
   other: [],
 }
