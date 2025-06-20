99fe041c1b21616529259e9684f39db8713b19e5
Super Jump <superjump.void@gmail.com>
2025-06-21 02:13:12 +0800

    logo

--- src/pages/07/m5s/_data/thanks.ts
+++ src/pages/07/m5s/_data/thanks.ts
@@ -1,5 +1,5 @@
 export const thanksList = {
-  groupList: ['MMW', 'SPJP'],
+  groupList: ['XivStrat', 'MMW', 'SPJP'],
   devList: ['SuperJump', 'Hikaru', 'DavidKitano', 'BaiXi', 'LuoLuoKe', 'YuKa'],
   other: [
     {
