065ef585f1cdb27e452e6749014c5767fcd445f3
Kitano <kitano@influx.io>
2025-06-24 16:29:42 +0800

    feat: add thank dev

--- src/pages/07/zelenia/_data/thanks.ts
+++ src/pages/07/zelenia/_data/thanks.ts
@@ -1,5 +1,5 @@
 export const thanksList = {
   groupList: ['XivStrat', 'ZiYan'],
-  devList: ['SuperJump', 'Hikaru', 'DavidKitano', 'LuoLuoKe', 'Sui', 'Demon'],
+  devList: ['SuperJump', 'Hikaru', 'DavidKitano', 'LuoLuoKe', 'Sui', 'Demon', 'Ling'],
   other: [],
 }
