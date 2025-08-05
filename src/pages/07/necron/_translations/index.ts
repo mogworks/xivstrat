c8779b633183abbc87d747d882847f456c5a056d
Super Jump <superjump.void@gmail.com>
2025-08-05 22:41:01 +0800

    7.3 5/n

--- src/pages/07/necron/_translations/index.ts
+++ src/pages/07/necron/_translations/index.ts
@@ -1,1 +1,12 @@
-export * from './en'
+import * as en from './en'
+import * as jp from './jp'
+
+// 创建合并字典，将英文和日文值拼接
+export const combinedTranslations = Object.keys(en.translations).reduce(
+  (acc, key) => {
+    const strKey = key as keyof typeof en.translations
+    acc[strKey] = `${en.translations[strKey]} / ${jp.translations[strKey]}`
+    return acc
+  },
+  {} as typeof en.translations
+)
