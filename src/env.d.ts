0549272273c0885944bf00c5415a843dd65ea6d2
Super Jump <superjump.void@gmail.com>
2026-01-28 18:58:04 +0800

    add more shadcn-vue components

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,7 @@
 /** biome-ignore-all lint/correctness/noUnusedVariables: https://docs.astro.build/en/guides/environment-variables/#intellisense-for-typescript */
 
 interface ImportMetaEnv {
+  readonly PUBLIC_SERVER_URL: string
   readonly PUBLIC_COS_BASE_URL: string
 }
 
