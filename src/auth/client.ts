7bcebdec8c91a7fd949fbb46b59f5c194b2a017a
Super Jump <superjump.void@gmail.com>
2026-01-30 23:46:49 +0800

    fix env

--- src/auth/client.ts
+++ src/auth/client.ts
@@ -1,8 +1,16 @@
 import { createAuthClient } from 'better-auth/client'
 import { localization } from 'better-auth-localization'
 
+const getBaseURL = () => {
+  if (typeof import.meta !== 'undefined' && import.meta.env?.PUBLIC_SERVER_URL) {
+    return import.meta.env.PUBLIC_SERVER_URL
+  }
+  // ssr 情况下 middleware 会在 server 端运行，所以这里不能用 import.meta
+  return process.env.PUBLIC_SERVER_URL!
+}
+
 export const authClient = createAuthClient({
-  baseURL: import.meta.env.PUBLIC_SERVER_URL,
+  baseURL: getBaseURL(),
   plugins: [
     localization({
       defaultLocale: 'zh-Hans',
